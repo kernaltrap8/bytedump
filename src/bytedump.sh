7488523aa745fd0f8aff38689ba5ce253efe43c6
kernaltrap <kernaltrap@gmail.com>
2024-07-09 19:52:39 -0500

    initial version
    Signed-off-by: kernaltrap <kernaltrap@gmail.com>

---
+++ src/bytedump.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# badvideo Copyright (C) 2024 kernaltrap8
+# This program comes with ABSOLUTELY NO WARRANTY
+# This is free software, and you are welcome to redistribute it
+# under certain conditions
+
+INPUT="$1"
+TEMP_FILE="/tmp/temp.bin"
+ARCH="i386:x86-64"
+
+if [[ "$INPUT" == "ppc64" ]]; then
+	INPUT="$2"
+	ARCH="powerpc:common64"
+fi
+
+echo -n "$INPUT" | xxd -r -p > "$TEMP_FILE"
+objdump -D -b binary -m "$ARCH" "$TEMP_FILE"
+rm "$TEMP_FILE"
